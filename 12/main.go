7aad766fa441981d33a78c5c228d392dd933b666
Filip Šuste <fsuste@gmail.com>
2023-12-12 21:41:33 +0100

    Finish lvl 12

--- 12/main.go
+++ 12/main.go
@@ -14,13 +14,7 @@ type Line struct {
 	groups []int
 }
 
-type Memo struct {
-	idx  int
-	curr string
-	line *Line
-}
-
-var mem = make(map[Memo]int)
+var memo [200][200][200]int
 
 func toInt(str string) int {
 	num, err := strconv.Atoi(str)
@@ -38,14 +32,6 @@ func parseLine(str string) Line {
 	}
 	return Line{parts[0], groups}
 }
-
-func solve(line Line) int {
-	res := 0
-	res += backtrack(0, "", line)
-
-	return res
-}
-
 func removeEmpty(str []string) []string {
 	var res []string
 	for _, s := range str {
@@ -56,44 +42,65 @@ func removeEmpty(str []string) []string {
 	return res
 }
 
-func backtrack(idx int, current string, line Line) int {
-	//if mem[Memo{idx, current, &line}] != 0 {
-	//	fmt.Println("Cache hit")
-	//	return mem[Memo{idx, current, &line}]
-	//}
-	count := 0
-	if idx == len(line.s_map) {
-		if good(Line{current, line.groups}) {
-			// fmt.Println(current)
-			count++
-		}
-		//mem[Memo{idx, current, &line}] = count
-		return count
-	}
-	if string(line.s_map[idx]) == "?" {
-		count += backtrack(idx+1, current+"#", line)
-		count += backtrack(idx+1, current+".", line)
-	}
+func solve(line Line) (int, int) {
+	res1 := solveLine(1, line.s_map, line.groups)
+	res2 := solveLine(5, line.s_map, line.groups)
 
-	return count + backtrack(idx+1, current+string(line.s_map[idx]), line)
+	return res1, res2
 }
 
-func good(line Line) bool {
-	if strings.Contains(line.s_map, "?") {
-		return false
+func solveLine(n int, str string, nums []int) int {
+	pattern := strings.Builder{}
+	numbers := []int{}
+	for i := 0; i < n; i++ {
+		pattern.WriteString(str)
+		if i != n-1 {
+			pattern.WriteRune('?')
+		}
+		numbers = append(numbers, nums...)
 	}
-	str_groups := strings.Split(line.s_map, ".")
-	str_groups = removeEmpty(str_groups)
-	if len(str_groups) != len(line.groups) {
-		return false
+
+	memo = [200][200][200]int{}
+	s := pattern.String()
+	return backtrack(s, 0, numbers, 0, 0)
+}
+
+func backtrack(str string, sidx int, numbers []int, nidx int, group_size int) int {
+	if len(str) == sidx { // if on the end of the string
+		// fmt.Println("End of string")
+		if nidx == len(numbers)-1 && numbers[nidx] == group_size { // valid string
+			//fmt.Println("1. Valid, adding 1")
+			return 1
+		}
+		if nidx == len(numbers) && group_size == 0 { // valid string
+			//fmt.Println("2. Valid, adding 1")
+			return 1
+		}
+		// fmt.Println("Invalid string :", str, " - ", numbers)
+		return 0 // invalid string
 	}
-	viable := true
-	for i, n := range line.groups {
-		if len(str_groups[i]) != n {
-			viable = false
+	// fmt.Println("Passing to calculate: ", str, " - ", numbers)
+
+	if memo[sidx][nidx][group_size] != 0 {
+		return memo[sidx][nidx][group_size] - 1 // -1 for special case of last group
+	}
+
+	sum := 0
+
+	if string(str[sidx]) == "?" || string(str[sidx]) == "#" { // opens or continues a group
+		sum += backtrack(str, sidx+1, numbers, nidx, group_size+1)
+	}
+	if string(str[sidx]) == "?" || string(str[sidx]) == "." { //check if I can close a group
+		if group_size > 0 && nidx < len(numbers) && numbers[nidx] == group_size { // group exists, group right size and not last
+			sum += backtrack(str, sidx+1, numbers, nidx+1, 0)
+		}
+		if group_size == 0 {
+			sum += backtrack(str, sidx+1, numbers, nidx, 0)
 		}
 	}
-	return viable
+
+	memo[sidx][nidx][group_size] = sum + 1
+	return sum
 }
 
 func main() {
@@ -111,12 +118,14 @@ func main() {
 		mat = append(mat, parseLine(scanner.Text()))
 	}
 
-	res := 0
+	res1 := 0
+	res2 := 0
 	for _, l := range mat {
-		c := solve(l)
-		fmt.Println(l, " - ", c)
-		res += c
+		s1, s2 := solve(l)
+		res1 += s1
+		res2 += s2
 	}
 
-	fmt.Println(res)
+	fmt.Println("Part 1: ", res1)
+	fmt.Println("Part 2: ", res2)
 }
